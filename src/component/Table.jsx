4735eb0b30caa27eba9b0d7551f5927b6f275e0a
Harrison <monteiro4100@gmail.com>
2022-10-19 17:03:02 -0300

    requisito 1

---
+++ src/component/Table.jsx
@@ -0,0 +1,55 @@
+import { useState, useEffect } from 'react';
+
+function Table() {
+  const [planets, setPlanets] = useState([]);
+  useEffect(() => {
+    const fetchPlanets = async () => {
+      const url = 'https://swapi.dev/api/planets';
+      const response = await fetch(url);
+      const data = await response.json();
+      setPlanets(data.results);
+    };
+    fetchPlanets();
+    console.log(planets);
+  }, []);
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>climate</th>
+          <th>created</th>
+          <th>diameter</th>
+          <th>edited</th>
+          <th>films</th>
+          <th>gravity</th>
+          <th>name</th>
+          <th>orbital period</th>
+          <th>population</th>
+          <th>rotation period</th>
+          <th>surface water</th>
+          <th>terrain</th>
+          <th>url</th>
+        </tr>
+        {planets.map((planet) => (
+          <tr key={ planet.name }>
+            <td>{planet.climate}</td>
+            <td>{planet.created}</td>
+            <td>{planet.diameter}</td>
+            <td>{planet.edited}</td>
+            <td>{planet.films}</td>
+            <td>{planet.gravity}</td>
+            <td>{planet.name}</td>
+            <td>{planet.orbital_period}</td>
+            <td>{planet.population}</td>
+            <td>{planet.rotation_period}</td>
+            <td>{planet.surface_water}</td>
+            <td>{planet.terrain}</td>
+            <td>{planet.url}</td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  );
+}
+
+export default Table;
