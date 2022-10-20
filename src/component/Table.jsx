cc215a1cdb265dcff59a25b460de04a14435a208
Harrison <monteiro4100@gmail.com>
2022-10-20 15:10:12 -0300

    requisito 3

--- src/component/Table.jsx
+++ src/component/Table.jsx
@@ -4,6 +4,49 @@ function Table() {
   const [planets, setPlanets] = useState([]);
   const [pesquisa, setPesquisa] = useState();
   const [backupPlanets, setBackupPlanets] = useState([]);
+  const [columFilter, setColumFilter] = useState('population');
+  const [comparisonFilter, setComparisonFilter] = useState('maior que');
+  const [valueFilter, setValueFilter] = useState(0);
+
+  const handleColumFilter = ({ target }) => {
+    setColumFilter(target.value);
+  };
+  const handleComparisonFilter = ({ target }) => {
+    setComparisonFilter(target.value);
+  };
+  const handleValueFilter = ({ target }) => {
+    setValueFilter(target.value);
+  };
+
+  const maiorOuMenorOuIgual = (parametro) => {
+    if (comparisonFilter === 'maior que') {
+      const filterPlanets = backupPlanets
+        .filter((planet) => Number(planet[parametro]) > Number(valueFilter));
+      setPlanets(filterPlanets);
+      console.log(parametro);
+    } else if (comparisonFilter === 'menor que') {
+      const filterPlanets = backupPlanets
+        .filter((planet) => Number(planet[parametro]) < Number(valueFilter));
+      setPlanets(filterPlanets);
+    } else {
+      const filterPlanets = backupPlanets
+        .filter((planet) => Number(planet[parametro]) === Number(valueFilter));
+      setPlanets(filterPlanets);
+    }
+  };
+  const onClick = () => {
+    if (columFilter === 'population') {
+      maiorOuMenorOuIgual('population');
+    } else if (columFilter === 'orbital_period') {
+      maiorOuMenorOuIgual('orbital_period');
+    } else if (columFilter === 'diameter') {
+      maiorOuMenorOuIgual('diameter');
+    } else if (columFilter === 'rotation_period') {
+      maiorOuMenorOuIgual('rotation_period');
+    } else if (columFilter === 'surface_water') {
+      maiorOuMenorOuIgual('surface_water');
+    }
+  };
 
   const handleChange = ({ target }) => {
     setPesquisa(target.value);
@@ -33,6 +76,35 @@ function Table() {
         value={ pesquisa }
         data-testid="name-filter"
       />
+      <select
+        data-testid="column-filter"
+        onChange={ handleColumFilter }
+        value={ columFilter }
+      >
+        <option value="population">population</option>
+        <option value="orbital_period">orbital_period</option>
+        <option value="diameter">diameter</option>
+        <option value="rotation_period">rotation_period</option>
+        <option value="surface_water">surface_water</option>
+      </select>
+      <select
+        data-testid="comparison-filter"
+        onChange={ handleComparisonFilter }
+        value={ comparisonFilter }
+      >
+        <option value="maior que">maior que</option>
+        <option value="menor que">menor que</option>
+        <option value="igual a">igual a</option>
+      </select>
+      <input
+        type="number"
+        data-testid="value-filter"
+        value={ valueFilter }
+        onChange={ handleValueFilter }
+      />
+      <button data-testid="button-filter" type="button" onClick={ onClick }>
+        adicionar filtro
+      </button>
       <table>
         <tr>
           <th>climate</th>
@@ -70,5 +142,4 @@ function Table() {
     </div>
   );
 }
-
 export default Table;
